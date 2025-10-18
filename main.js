d3e22a50dc92568c13d5628899e53f8807d23853
Veneil Sai Indla <saiveneil21@gmail.com>
2025-10-18 19:30:56 +0530

    New version

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 var typed = new Typed(".typed", {
-    strings: ["Mechanical Engineer","Researcher"],
+    strings: ["Mechanical Engineer","Design Researcher","Computational Mechanics Enthusiast"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
     loop: true,
   });
+ 
